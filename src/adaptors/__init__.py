da505e5e910cd24b8d96430d8b1dbeeef3e1b6fc
linfeng-du <linfeng9918@gmail.com>
2024-12-04 19:31:03 -0500

    update adaptors

--- src/adaptors/__init__.py
+++ src/adaptors/__init__.py
@@ -4,9 +4,10 @@ from .lora import LoRAController
 from .mixture_of_loras import MixtureOfLoRAsController
 
 
-def get_controller(adaptor: str, **adaptor_kwargs: Any) -> (
-    LoRAController | MixtureOfLoRAsController
-):
+Controller = LoRAController | MixtureOfLoRAsController
+
+
+def get_controller(adaptor: str, **adaptor_kwargs: Any) -> Controller:
     controller_classes = {
         'lora': LoRAController,
         'mixture_of_loras': MixtureOfLoRAsController
